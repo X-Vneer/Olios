f2af9d3468e9b37243732ed60afc8c5670c5eb54
X_V.neer <xv.neer@gmail.com>
2022-09-17 17:01:00 +0300

    seperated each section in it's own page

--- js/index.js
+++ js/index.js
@@ -1,14 +1,3 @@
-// side bar items
-const sideBarIcons = document.querySelectorAll("svg");
-sideBarIcons.forEach((ele, ind) => {
-  ele.addEventListener("click", (e) => {
-    sideBarIcons.forEach((ele) => {
-      ele.classList.remove("active");
-    });
-    ele.classList.add("active");
-  });
-});
-
 // right bar
 const btn = document.getElementById("menu-btn");
 const sideMenuItems = document.getElementById("side-menu-items");
@@ -69,31 +58,3 @@ like.forEach((ele) =>
         : --likesNumber.textContent);
   })
 );
-
-// highlighting icons
-const handleSectionsView = (sections) => {
-  let ind;
-  sections.forEach((section) => {
-    if (section.isIntersecting) {
-      sideBarIcons.forEach((ele) => ele.classList.remove("active"));
-      ind =
-        section.target.id === "home"
-          ? 0
-          : section.target.id === "products"
-          ? 1
-          : 2;
-
-      sideBarIcons[ind].classList.add("active");
-    }
-  });
-};
-let sectionObsorver = new IntersectionObserver(handleSectionsView, {
-  threshold: 0.2,
-});
-const home = document.getElementById("home");
-const products = document.getElementById("products");
-const search = document.getElementById("search");
-
-sectionObsorver.observe(home);
-sectionObsorver.observe(products);
-sectionObsorver.observe(search);
